a5404cb8fb94124c49db8c3b83bfd1d6fe9f41b9
kriston <>
2000-08-01 20:46:49 +0000

    * nsd/adp.c (Ns_AdpRequest): The enableexpire option was putting         an Expires header even if it already existed.

--- nsd/adp.c
+++ nsd/adp.c
@@ -2,7 +2,7 @@
  * The contents of this file are subject to the AOLserver Public License
  * Version 1.1 (the "License"); you may not use this file except in
  * compliance with the License. You may obtain a copy of the License at
- * http://aolserver.lcs.mit.edu/.
+ * http://aolserver.com/.
  *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
@@ -57,7 +57,7 @@
  * ns_param   "ParserName" "utf8"
  */
 
-static const char *RCSID = "@(#) $Header: /Users/dossy/Desktop/cvs/aolserver/nsd/Attic/adp.c,v 1.2 2000/05/02 14:39:30 kriston Exp $, compiled: " __DATE__ " " __TIME__;
+static const char *RCSID = "@(#) $Header: /Users/dossy/Desktop/cvs/aolserver/nsd/Attic/adp.c,v 1.3 2000/08/01 20:46:49 kriston Exp $, compiled: " __DATE__ " " __TIME__;
 
 #include "nsd.h"
 
@@ -1506,7 +1506,7 @@ Ns_AdpRequest(Ns_Conn *conn, char *file)
 
 	default:
 	    if (nsconf.adp.enableexpire) {
-		Ns_ConnSetHeaders(conn, "Expires", "now");
+		Ns_ConnCondSetHeaders(conn, "Expires", "now");
 	    }
 	    if (Ns_ConnResponseStatus(conn) == 0) {
                 status = Ns_ConnReturnData(conn, 200,
