80eca67180d6d435ad34aba365db62d2e7247e04
kriston <>
2000-10-23 15:04:14 +0000

    upped version to 3.3pre

--- nsd/nsd.h
+++ nsd/nsd.h
@@ -91,8 +91,8 @@
  */
 
 #define NSD_NAME             "AOLserver"
-#define NSD_VERSION	     "3.2"
-#define NSD_LABEL            "aolserver3_2"
+#define NSD_VERSION	     "3.3pre"
+#define NSD_LABEL            "aolserver3_3"
 #define NSD_TAG              "$Name:  $"
 #define NS_CONFIG_PARAMETERS "ns/parameters"
 #define NS_CONFIG_SERVERS    "ns/servers"
