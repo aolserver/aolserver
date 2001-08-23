7e59d1d677617fdf04078120d1cd710b8dc7fcb1
kriston <>
2001-08-23 20:52:20 +0000

    changed label name

--- nsd/nsd.h
+++ nsd/nsd.h
@@ -91,8 +91,8 @@
  */
 
 #define NSD_NAME             "AOLserver"
-#define NSD_VERSION	     "3.2"
-#define NSD_LABEL            "aolserver3_2"
+#define NSD_VERSION	     "3.2.1"
+#define NSD_LABEL            "aolserver3_2_1"
 #define NSD_TAG              "$Name:  $"
 #define NS_CONFIG_PARAMETERS "ns/parameters"
 #define NS_CONFIG_SERVERS    "ns/servers"
