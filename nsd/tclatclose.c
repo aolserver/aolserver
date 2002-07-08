f4834ffb1b97ae26deced16196698f110554e1d2
jgdavidson <>
2002-07-08 02:51:10 +0000

    Removed NsTclAtCloseObjCmd for now.

--- nsd/tclatclose.c
+++ nsd/tclatclose.c
@@ -33,7 +33,7 @@
  *	Routines for the ns_atclose command.
  */
 
-static const char *RCSID = "@(#) $Header: /Users/dossy/Desktop/cvs/aolserver/nsd/Attic/tclatclose.c,v 1.3 2002/06/13 04:41:21 jcollins Exp $, compiled: " __DATE__ " " __TIME__;
+static const char *RCSID = "@(#) $Header: /Users/dossy/Desktop/cvs/aolserver/nsd/Attic/tclatclose.c,v 1.4 2002/07/08 02:51:10 jgdavidson Exp $, compiled: " __DATE__ " " __TIME__;
 
 #include "nsd.h"
 
@@ -100,14 +100,14 @@ NsTclAtCloseCmd(ClientData arg, Tcl_Interp *interp, int argc, char **argv)
     AtClose *atPtr;
 
     if (argc < 2 || argc > 3) {
-	Tcl_AppendResult (interp, "wrong # args: should be \"",
-			  argv[0], " { script | procname ?arg? }\"", NULL);
+	Tcl_AppendResult(interp, "wrong # args: should be \"",
+			 argv[0], "script ?arg?\"", NULL);
 	return TCL_ERROR;
     }
     if (argc == 2) {
 	script = argv[1];
     } else {
-	script = Tcl_Concat(2, argv+1);
+	script = Tcl_Concat(2, (const char **) argv+1);
     }
 
     /*
@@ -121,56 +121,6 @@ NsTclAtCloseCmd(ClientData arg, Tcl_Interp *interp, int argc, char **argv)
     itPtr->firstAtClosePtr = atPtr;
     if (script != argv[1]) {
 	ckfree(script);
-    }
-    return TCL_OK;
-}
-
-
-/*
- *----------------------------------------------------------------------
- *
- * NsTclAtCloseObjCmd --
- *
- *	Implements ns_atclose as obj command. 
- *
- * Results:
- *	Tcl result. 
- *
- * Side effects:
- *	See docs. 
- *
- *----------------------------------------------------------------------
- */
-
-int
-NsTclAtCloseObjCmd(ClientData arg, Tcl_Interp *interp, int objc, Tcl_Obj *CONST objv[])
-{
-    NsInterp *itPtr = arg;
-    char    *script;
-    AtClose *atPtr;
-
-    if (objc < 2 || objc > 3) {
-        Tcl_WrongNumArgs(interp, 1, objv, "{ script | procname ?arg? }");
-	return TCL_ERROR;
-    }
-
-    if (objc == 2) {
-	script = Tcl_GetString(objv[1]);
-    } else {
-	script = Tcl_GetString(Tcl_ConcatObj(2, objv+1));
-    }
-
-    /*
-     * Push the script onto the head of the atclose list so scripts
-     * will be called in reversed order when invoked.
-     */
-
-    atPtr = ns_malloc(sizeof(AtClose) + strlen(script));
-    strcpy(atPtr->script, script);
-    atPtr->nextPtr = itPtr->firstAtClosePtr;
-    itPtr->firstAtClosePtr = atPtr;
-    if (script != Tcl_GetString(objv[1])) {
-		ckfree(script);
     }
     return TCL_OK;
 }
