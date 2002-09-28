8190357a1286d617d0bae54093b6e62cae50cb25
jgdavidson <>
2002-09-28 19:24:26 +0000

    Added NsInitQueue to support AOLserver 3.x Ns_GetConn. Also implemented AOLserver 3.x Ns_QueueConn stub (returns NS_ERROR).

--- nsd/queue.c
+++ nsd/queue.c
@@ -34,7 +34,7 @@
  *	and service threads.
  */
 
-static const char *RCSID = "@(#) $Header: /Users/dossy/Desktop/cvs/aolserver/nsd/queue.c,v 1.14 2002/09/21 17:55:45 jgdavidson Exp $, compiled: " __DATE__ " " __TIME__;
+static const char *RCSID = "@(#) $Header: /Users/dossy/Desktop/cvs/aolserver/nsd/queue.c,v 1.15 2002/09/28 19:24:26 jgdavidson Exp $, compiled: " __DATE__ " " __TIME__;
 
 #include "nsd.h"
 
@@ -60,6 +60,84 @@ static void JoinConnThread(Ns_Thread *threadPtr);
 static void AppendConn(Tcl_DString *dsPtr, Conn *connPtr, char *state);
 static void AppendConnList(Tcl_DString *dsPtr, Conn *firstPtr,
     	char *state);
+
+/*
+ * Static variables defined in this file.
+ */
+
+static Ns_Tls argtls;
+
+
+/*
+ *----------------------------------------------------------------------
+ *
+ * NsInitQueue --
+ *
+ *	Init connection queue.
+ *
+ * Results:
+ *	None.
+ *
+ * Side effects:
+ *	None.
+ *
+ *----------------------------------------------------------------------
+ */
+
+void
+NsInitQueue(void)
+{
+    Ns_TlsAlloc(&argtls, NULL);
+}
+
+
+/*
+ *----------------------------------------------------------------------
+ *
+ * Ns_QueueConn --
+ *
+ *	Queue a connection from a loadable driver (no longer supported).
+ *
+ * Results:
+ *	NS_ERROR.
+ *
+ * Side effects:
+ *	None.
+ *
+ *----------------------------------------------------------------------
+ */
+
+int
+Ns_QueueConn(void *drv, void *arg)
+{
+    return NS_ERROR;
+}
+
+
+/*
+ *----------------------------------------------------------------------
+ *
+ * Ns_GetConn --
+ *
+ *	Return the current connection in this thread.
+ *
+ * Results:
+ *	Pointer to conn or NULL.
+ *
+ * Side effects:
+ *	None.
+ *
+ *----------------------------------------------------------------------
+ */
+
+Ns_Conn *
+Ns_GetConn(void)
+{
+    Arg *argPtr;
+
+    argPtr = Ns_TlsGet(&argtls);
+    return (argPtr ? ((Ns_Conn *) argPtr->connPtr) : NULL);
+}
 
 
 /*
@@ -436,6 +514,7 @@ NsConnThread(void *arg)
      * Set the conn thread name.
      */
 
+    Ns_TlsSet(&argtls, argPtr);
     Ns_MutexLock(&servPtr->queue.lock);
     id = servPtr->threads.nextid++;
     Ns_MutexUnlock(&servPtr->queue.lock);
