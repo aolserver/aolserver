5337e11a784dbbab50dad83d398be5d2faf8603e
shmooved <>
2003-04-22 18:17:55 +0000

    Fixed bug causing first two string args to "ns_log" to be improperly concatenated together.

--- nsd/log.c
+++ nsd/log.c
@@ -34,7 +34,7 @@
  *	Manage the server log file.
  */
 
-static const char *RCSID = "@(#) $Header: /Users/dossy/Desktop/cvs/aolserver/nsd/log.c,v 1.22 2003/03/07 18:08:28 vasiljevic Exp $, compiled: " __DATE__ " " __TIME__;
+static const char *RCSID = "@(#) $Header: /Users/dossy/Desktop/cvs/aolserver/nsd/log.c,v 1.23 2003/04/22 18:17:55 shmooved Exp $, compiled: " __DATE__ " " __TIME__;
 
 #include "nsd.h"
 
@@ -457,7 +457,7 @@ NsTclLogObjCmd(ClientData arg, Tcl_Interp *interp, int objc,
     if (LogStart(cachePtr, severity)) {
 	for (i = 2; i < objc; ++i) {
 	    Ns_DStringVarAppend(&cachePtr->buffer,
-		Tcl_GetString(objv[i]), i > 2 ? " " : NULL, NULL);
+		Tcl_GetString(objv[i]), i > 1 ? " " : NULL, NULL);
 	}
 	LogEnd(cachePtr);
     }
