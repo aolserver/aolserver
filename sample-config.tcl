67580d77ccfadec0dd453e2654ab37edd7426ed8
scottg <>
2003-12-28 01:18:21 +0000

    Fixed bug #805761.

--- sample-config.tcl
+++ sample-config.tcl
@@ -27,7 +27,7 @@
 # version of this file under either the License or the GPL.
 # 
 #
-# $Header: /Users/dossy/Desktop/cvs/aolserver/Attic/sample-config.tcl,v 1.11 2003/08/08 14:52:17 elizthom Exp $
+# $Header: /Users/dossy/Desktop/cvs/aolserver/Attic/sample-config.tcl,v 1.12 2003/12/28 01:18:21 scottg Exp $
 #
 
 #
@@ -83,12 +83,12 @@ ns_param   debug           false
 #ns_param HackContentType false       ;# automatic adjustment of response
                                        # content-type header to include charset
                                        # This defaults to True.
-ns_param  OutputCharset  iso8859-1    ;# Default output charset.  When none specified,
+ns_param  OutputCharset  iso-8859-1    ;# Default output charset.  When none specified,
                                        # no character encoding of output is performed.
-ns_param  URLCharset     iso8859-1    ;# Default Charset for Url Encode/Decode.
+ns_param  URLCharset     iso-8859-1    ;# Default Charset for Url Encode/Decode.
                                        # When none specified, no character set encoding
                                        # is performed.
-#ns_param  PreferredCharsets { utf-8 iso8859-1 } ;# This parameter supports output
+#ns_param  PreferredCharsets { utf-8 iso-8859-1 } ;# This parameter supports output
                                        # encoding arbitration.
 
 #
