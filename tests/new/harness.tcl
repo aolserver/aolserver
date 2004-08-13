e3fd9e44d4d5d06d2a2fdae4cc580e002643a62d
dossy <>
2004-08-13 15:00:54 +0000

    Really basic test harness for automating tests.

---
+++ tests/new/harness.tcl
@@ -0,0 +1,24 @@
+proc test {script} {
+    set ::assertionCount 0
+    if {[set res [catch [list eval $script] msg]]} {
+        ns_adp_puts -nonewline "FAIL\n$::assertionCount assertions\n$msg"
+    } else {
+        ns_adp_puts -nonewline "PASS\n$::assertionCount assertions"
+    }
+}
+
+proc assert {true message} {
+    if {!$true} {
+        error $message
+    }
+}
+
+proc assertEquals {expected actual {message ""}} {
+    incr ::assertionCount
+    if {![string length $message]} {
+        set message "no description provided"
+    }
+    set message "(#$::assertionCount) $message"
+    assert [string equal $expected $actual] \
+        "$message\n  Expected: $expected\n    Actual: $actual"
+}
