c8c03bd9ed0a84666499e21c634ee04211482a29
jgdavidson <>
2005-08-19 00:50:12 +0000

    Added -f option to create a specific filename.

--- util/nsinstall.tcl
+++ util/nsinstall.tcl
@@ -32,32 +32,54 @@
 #
 #	Install script for AOLserver.
 #
-# Usage:
-#	nsinstall.tcl -d <dst> ?-n? -?-e? pattern ?pattern pattern ...?
-#
-# Where:
-#	-d <dst>	Destination directory (required).
-#	-n		Do not overwrite existing files.
-#	-e		Install with execute permission.
-#	pattern		One or more file glob patterns (e.g., foo.c, *.tcl)
-#
 
 
+proc usage {{status 1}} {
+	puts {Usage:
+
+	nsinstall.tcl ?-d dir? ?-f file? ?-n? -?-e? ?--? pattern ?pattern ...?
+
+Where:
+	-h		This message.
+	-d dir		Destination directory.
+	-f file		Copy as specific filename.
+	-n		Do not overwrite existing files.
+	-e		Install with execute permission.
+	--		Specifies end of args.
+	pattern		One or more file glob patterns (e.g., foo.c, *.tcl)
+
+One of -f or -d must be specified}
+	exit $status
+}
+
+
+set pid [pid]
 set mode 0644
 set overwrite 1
-set done 0
-set i 0
 set iswin [string equal $tcl_platform(platform) windows]
+if $iswin {
+	set modestr ""
+} else {
+	set modestr " ($mode)"
+}
+
 
 #
-# Process arguments.
+# Process option arguments.
 #
 
+set i 0
 while {$i < $argc} {
 	set arg [lindex $argv $i]
 	switch -glob -- $arg {
+		-h {
+			usage 0
+		}
 		-d {
 			set dir [lindex $argv [incr i]]
+		}
+		-f {
+			set file [lindex $argv [incr i]]
 		}
 		-e {
 			set mode 0755
@@ -66,58 +88,75 @@ while {$i < $argc} {
 			set overwrite 0
 		}
 		-* {
-			puts "Unknown option: $arg"
-			exit 1
+			usage
 		}
+		-- -
 		default {
-			set done 1
+			break
 		}
 	}
-	if $done break
+	incr i
+}
+
+#
+# Get list of files from remaining args.
+#
+
+set files ""
+while {$i < $argc} {
+	foreach f [glob -nocomplain [lindex $argv $i]] {
+		lappend files $f
+	}
 	incr i
 }
 
 
 #
-# Create destination directory.
+# Sanity checks.
 #
 
-if ![info exists dir] {
-	puts "Missing required -d <dir> arg"
-	exit 1
+if [info exists file] {
+	if {[llength $files] > 1} {
+		usage
+	}
+	set file [file normalize $file]
+	set dir [file dirname $file]
+	if {[file exists $dir] && ![file isdirectory $dir]} {
+		usage
+	}
 }
+if ![info exists dir] {
+	usage
+}
+
+
+#
+# Create directory and copy file(s).
+#
+
 file mkdir $dir
 
-
-#
-# Copy files specified by each glob pattern.
-#
-
-if $iswin {
-	set modestr ""
-} else {
-	set modestr " ($mode)"
-}
-set pid [pid]
-while {$i < $argc} {
-	foreach src [glob -nocomplain [lindex $argv $i]] {
-		set src [file normalize $src]
+foreach src $files {
+	set src [file normalize $src]
+	if [info exists file] {
+		set tail [file tail $file]
+	} else {
 		set tail [file tail $src]
-		set dst [file normalize $dir/$tail]
-		set tmp [file normalize $dir/.#inst.$tail.$pid]
-		incr i
-		if [file exists $dst] {
-			if !$overwrite {
-				puts "exists: $dst"
-				continue
-			}
-			file delete $dst
-		}
-		file copy $src $tmp
-		if {!$iswin} {
-			file attributes $tmp -permission $mode
-		}
-		file rename $tmp $dst
-		puts "installed: $dst$modestr"
 	}
+	set dst [file normalize $dir/$tail]
+	set tmp [file normalize $dir/.#inst.$tail.$pid]
+	incr i
+	if [file exists $dst] {
+		if !$overwrite {
+			puts "exists: $dst"
+			continue
+		}
+		file delete $dst
+	}
+	file copy $src $tmp
+	if {!$iswin} {
+		file attributes $tmp -permission $mode
+	}
+	file rename $tmp $dst
+	puts "installed: $dst$modestr"
 }
