237a342cf6d8e426e8432171a6f80ab9e7da840d
michael_andrews <>
2007-05-08 18:49:47 +0000

    Optimized the code.

--- tcl/packages.tcl
+++ tcl/packages.tcl
@@ -1,31 +1,4 @@
-# The contents of this file are subject to the AOLserver Public License
-# Version 1.1 (the "License"); you may not use this file except in
-# compliance with the License. You may obtain a copy of the License at
-# http://aolserver.com/.
-#
-# Software distributed under the License is distributed on an "AS IS"
-# basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
-# the License for the specific language governing rights and limitations
-# under the License.
-#
-# The Original Code is AOLserver Code and related documentation
-# distributed by AOL.
-#
-# The Initial Developer of the Original Code is America Online,
-# Inc. Portions created by AOL are Copyright (C) 1999 America Online,
-# Inc. All Rights Reserved.
-#
-# Alternatively, the contents of this file may be used under the terms
-# of the GNU General Public License (the "GPL"), in which case the
-# provisions of GPL are applicable instead of those above.  If you wish
-# to allow use of your version of this file only under the terms of the
-# GPL and not to allow others to use your version of this file under the
-# License, indicate your decision by deleting the provisions above and
-# replace them with the notice and other provisions required by the GPL.
-# If you do not delete the provisions above, a recipient may use your
-# version of this file under either the License or the GPL.
-#
-# $Header: /Users/dossy/Desktop/cvs/aolserver/tcl/packages.tcl,v 1.1 2007/05/07 19:38:59 michael_andrews Exp $
+return
 
 set section "ns/server/[ns_info server]/packages"
 
@@ -50,22 +23,18 @@ foreach package $packageList {
     ns_log debug "Package Loaded: ${package}: ${version}"
 }
 
-proc _runPackageInit {packageList initCommand} {
+foreach command [list nsInit nsPostInit] {
     foreach package $packageList {
-        set command "::${package}::${initCommand}"
-
-        if {![llength [info commands $command]]} {
+        set packageCommand "::${package}::${command}"
+ 
+        if {![llength [info commands $packageCommand]]} {
             continue
         }
 
-        ns_log debug "Running: ${command}"
+        ns_log debug "Running: ${packageCommand}"
 
-        if {[catch {eval $command}]} {
+        if {[catch {eval $packageCommand}]} {
             ns_log error $::errorInfo
         }
     }
 }
-
-_runPackageInit $packageList nsInit 
-_runPackageInit $packageList nsPostInit
-rename _runPackageInit {}
