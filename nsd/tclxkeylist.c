650ff5697c8b423f7cc338fbca64ac7f1d891e99
Christopher Chavez <chrischavez@gmx.us>
2022-10-11 02:03:23 -0500

    Migrate to Tcl_Panic() (#9)
    Replaces usage of Tcl panic() function which is deprecated
    and unavailable on macOS due to conflict with system panic()

--- nsd/tclxkeylist.c
+++ nsd/tclxkeylist.c
@@ -45,7 +45,7 @@ static const char *RCSID = "@(#) $Header: /Users/dossy/Desktop/cvs/aolserver/nsd
  */
 #ifdef TCLX_DEBUG
 #   define TclX_Assert(expr) ((expr) ? (void)0 : \
-                              panic("TclX assertion failure: %s:%d \"%s\"\n",\
+                              Tcl_Panic("TclX assertion failure: %s:%d \"%s\"\n",\
                                     __FILE__, __LINE__, "expr"))
 #else
 #   define TclX_Assert(expr)
